c831b6ca667d794f0aa3863e3ceacd4f062d54a2
fateekmash26 <fateekmashroor.amin@students.mq.edu.au>
2026-03-25 15:43:38 +1100

    flower head updated

--- src/comp3170/week5/sceneobjects/FlowerHead.java
+++ src/comp3170/week5/sceneobjects/FlowerHead.java
@@ -1,5 +1,7 @@
 package comp3170.week5.sceneobjects;
 
+import static org.lwjgl.opengl.GL41.*;
+
 import org.joml.Matrix4f;
 import org.joml.Vector3f;
 import org.joml.Vector4f;
@@ -8,6 +10,7 @@ import comp3170.GLBuffers;
 import comp3170.SceneObject;
 import comp3170.Shader;
 import comp3170.ShaderLibrary;
+
 public class FlowerHead extends SceneObject {
 	
 	private static final String VERTEX_SHADER = "vertex.glsl";
@@ -21,13 +24,33 @@ public class FlowerHead extends SceneObject {
 
 	public FlowerHead(int nPetals, Vector3f colour) {
 		
-		// TODO: Create the flower head. (TASK 1)
-		// Consider the best way to draw the mesh with the nPetals input. 
-		// Note that this may involve moving some code OUT of this class!
-		
 		shader = ShaderLibrary.instance.compileShader(VERTEX_SHADER, FRAGMENT_SHADER);		
 		
 		petalColour = colour;
+		
+		float innerRadius = 0.2f;
+		float outerRadius = 0.4f;
+		
+		int nVertices = nPetals * 2;
+		vertices = new Vector4f[nVertices];
+		
+		for (int i = 0; i < nVertices; i++) {
+			float angle = (float)(2 * Math.PI * i / nVertices);
+			
+			float radius;
+			if (i % 2 == 0) {
+				radius = outerRadius;
+			} else {
+				radius = innerRadius;
+			}
+			
+			float x = (float)(radius * Math.cos(angle));
+			float y = (float)(radius * Math.sin(angle));
+			
+			vertices[i] = new Vector4f(x, y, 0, 1);
+		}
+		
+		vertexBuffer = GLBuffers.createBuffer(vertices);
 	}
 
 	public void update(float dt) {
@@ -35,6 +58,11 @@ public class FlowerHead extends SceneObject {
 	}
 
 	public void drawSelf(Matrix4f mvpMatrix) {
-		// TODO: Add any appropriate draw code. (TASK 1)
+		shader.enable();
+		shader.setUniform("u_mvpMatrix", mvpMatrix);
+		shader.setAttribute("a_position", vertexBuffer);
+		shader.setUniform("u_colour", petalColour);
+		
+		glDrawArrays(GL_TRIANGLE_FAN, 0, vertices.length);
 	}
 }
